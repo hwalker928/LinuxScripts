c0fc64cb143220f0baa088978e732f2490de3c7b
hwalker928 <harry@repoleved.xyz>
2020-11-05 21:11:57 +0000

    Update minecraftServer.sh

--- minecraftServer.sh
+++ minecraftServer.sh
@@ -6,18 +6,21 @@ warn(){
     echo -e '\e[31m'$1'\e[0m';
 }
 
-output "Welcome to hwalker928's Minecraft Server Script!"
-output "Time to begin!"
+output "Welcome to hwalker928's Minecraft Server Script"
 output ""
 output "What should we call this server?"
-warn "Please note: it must be ONE word!"
+warn "Please note: It must be ONE word"
 read servername
-output "Making server folder!"
+output "Making server folder"
 mkdir $servername &> /dev/null
 cd $servername
-output "Created server folder!"
+output "Created server folder"
 
-output "Downloading tuinity-paperclip.jar!"
+output "Downloading tuinity-paperclip.jar"
 wget https://ci.codemc.io/job/Spottedleaf/job/Tuinity/lastSuccessfulBuild/artifact/tuinity-paperclip.jar &> /dev/null
-output "tuinity-paperclip.jar was downloaded!"
+output "tuinity-paperclip.jar was downloaded"
 
+output "Updating packages"
+sudo apt-get update -y
+sudo apt-get upgrade -y
+output "Finished upgrading packages"
