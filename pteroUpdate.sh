16e3f0b2acb3bd8c804c00fdaa5ec054f3cd85d1
hwalker928 <mail@harrydev.org>
2021-03-06 22:17:19 +0000

    Update pteroUpdate.sh

--- pteroUpdate.sh
+++ pteroUpdate.sh
@@ -1,11 +1,8 @@
 if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+  then echo "Please run as root (sudo su)"
   exit
 fi
-
-echo "Command 1 - Downloading binaries"
-curl -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_amd64
-echo "Command 2 - Modifying permissions"
-chmod u+x /usr/local/bin/wings
-echo "Command 3 - Restarting wings"
-systemctl restart wings
+curl -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_amd64 >> /tmp/wings.log
+chmod u+x /usr/local/bin/wings >> /tmp/wings.log
+systemctl restart wings >> /tmp/wings.log
+echo "Updated wings to v1.3.0"
