20b75a67d7952b6ca835e7d1fec33ad728e1139a
hwalker928 <mail@harrydev.org>
2021-03-06 22:11:19 +0000

    1.3.0

--- pteroUpdate.sh
+++ pteroUpdate.sh
@@ -1,3 +1,8 @@
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 echo "Command 1 - Downloading binaries"
 curl -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_amd64
 echo "Command 2 - Modifying permissions"
