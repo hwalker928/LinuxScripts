96e5f4a48e7da392a4a72682d21a21763830e4d1
hwalker928 <mail@harrydev.org>
2021-03-26 16:45:31 +0000

    Update wings.sh

--- wings.sh
+++ wings.sh
@@ -1,3 +1,10 @@
-curl -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_amd64
-chmod u+x /usr/local/bin/wings
-systemctl restart wings
+output(){
+    echo -e '\e[36m'$1'\e[0m';
+}
+output "Downloading new wings.."
+curl -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_amd64 > /dev/null 2>&1
+output "Applying permissions.."
+chmod u+x /usr/local/bin/wings > /dev/null 2>&1
+output "Restarting wings.."
+systemctl restart wings > /dev/null 2>&1
+output "Done!"
