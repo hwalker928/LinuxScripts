06d0f0ec5f25332d2cff57a4264d66719c831115
hwalker928 <freelance@harrydev.me>
2021-04-30 21:46:29 +0100

    Delete wingstestss.sh

--- wingstestss.sh
+++
@@ -1,9 +0,0 @@
-output(){
-    echo -e '\e[36m'$1'\e[0m';
-}
-
-if [[ $(wings version) =~ $(curl -s https://cdn.pterodactyl.io/releases/latest.json | jq -r '.wings') ]]; then
-    output "Latest wings"
-else
-    curl -s https://cdn.pterodactyl.io/releases/latest.json | jq -r '.wings'
-fi
