f289db4dcea0ceb889bca3c07b9cfcbe96c1fc1b
hwalker928 <freelance@harrydev.me>
2021-04-30 21:50:04 +0100

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -3,3 +3,19 @@ read domain
 systemctl stop nginx
 certbot certonly --standalone -d ${domain}
 systemctl start nginx
+
+output(){
+    echo -e '\e[36m'$1'\e[0m';
+}
+
+if [ $(whoami) = 'root' ]; then
+    output "Running as root.."
+    output "Enter domain to generate SSL certificate for:"
+    read domain
+    systemctl stop nginx
+    certbot certonly --standalone -d ${domain}
+    systemctl start nginx
+    output "Finished script."
+else
+    output "Please run this command as root."
+fi
