48a82f2deb500ead973fdc0cecba521bdf5fc139
hwalker928 <freelance@harrydev.me>
2021-04-30 21:18:29 +0100

    Update wings.sh

--- wings.sh
+++ wings.sh
@@ -4,7 +4,7 @@ output(){
 
 if [ $(whoami) = 'root' ]; then
     output "Running as root.."
-	output "Downloading new wings.."
+    output "Downloading new wings.."
     curl -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_amd64 > /dev/null 2>&1
     output "Applying permissions.."
     chmod u+x /usr/local/bin/wings > /dev/null 2>&1
