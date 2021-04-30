b2fd849e6fbf4cc485ef7dc5efdeaba8b4d435f6
hwalker928 <freelance@harrydev.me>
2021-04-30 21:48:05 +0100

    Update wings.sh

--- wings.sh
+++ wings.sh
@@ -3,11 +3,11 @@ output(){
 }
 
 if [ $(whoami) = 'root' ]; then
+    output "Running as root.."
     output "Checking if latest wings.."
     if [[ $(wings version) =~ $(curl -s https://cdn.pterodactyl.io/releases/latest.json | jq -r '.wings') ]]; then
         output "Already latest."
     else
-        output "Running as root.."
         output "Downloading new wings.."
         curl -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_amd64 > /dev/null 2>&1
         output "Applying permissions.."
