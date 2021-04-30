4b395b4a7ee1b467b7c87fdd7784ec194742a231
hwalker928 <freelance@harrydev.me>
2021-04-30 21:18:22 +0100

    Update wings.sh

--- wings.sh
+++ wings.sh
@@ -1,10 +1,18 @@
 output(){
     echo -e '\e[36m'$1'\e[0m';
 }
-output "Downloading new wings.."
-curl -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_amd64 > /dev/null 2>&1
-output "Applying permissions.."
-chmod u+x /usr/local/bin/wings > /dev/null 2>&1
-output "Restarting wings.."
-systemctl restart wings > /dev/null 2>&1
-output "Done!"
+
+if [ $(whoami) = 'root' ]; then
+    output "Running as root.."
+	output "Downloading new wings.."
+    curl -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_amd64 > /dev/null 2>&1
+    output "Applying permissions.."
+    chmod u+x /usr/local/bin/wings > /dev/null 2>&1
+    output "Restarting wings.."
+    systemctl restart wings > /dev/null 2>&1
+    output "Done!"
+else
+	output "Please run this command as root."
+fi
+
+
