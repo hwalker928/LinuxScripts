d0b3e109b2de2e6837df421de87cc2ba2f9f0b03
hwalker928 <freelance@harrydev.me>
2021-04-30 21:26:24 +0100

    Update wingstest.sh

--- wingstest.sh
+++ wingstest.sh
@@ -2,7 +2,7 @@ output(){
     echo -e '\e[36m'$1'\e[0m';
 }
 
-if [ $(wings version) = 'wings v1.4.1\nCopyright © 2018 - 2021 Dane Everitt & Contributors' ]; then
+if [ $(wings version) =~ "v1.4.1" ]; then
     output "Latest wings"
 else
     output "Please run this command as root."
