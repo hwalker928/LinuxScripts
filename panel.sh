affe79618f899198f9ffcc69f8bd94963d3bb0a9
hwalker928 <mail@harrydev.org>
2021-05-08 12:43:27 +0100

    Update panel.sh

--- panel.sh
+++ panel.sh
@@ -11,12 +11,18 @@ if [ $(whoami) = 'root' ]; then
     cd /var/www/pterodactyl
     php artisan down
     tar -xzvf panel.tar.gz
+    read -p "Press enter to continue or CTRL+C to abort"
     chmod -R 755 storage/* bootstrap/cache
+    read -p "Press enter to continue or CTRL+C to abort"
     composer require laravel/socialite
+    read -p "Press enter to continue or CTRL+C to abort"
     composer install --no-dev --optimize-autoloader
+    read -p "Press enter to continue or CTRL+C to abort"
     php artisan view:clear
     php artisan config:clear
+    read -p "Press enter to continue or CTRL+C to abort"
     php artisan migrate --seed --force
+    read -p "Press enter to continue or CTRL+C to abort"
     chown -R www-data:www-data /var/www/pterodactyl/*
     php artisan queue:restart
     php artisan up
