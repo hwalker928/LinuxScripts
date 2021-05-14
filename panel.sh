a2272f05e4d2571bd762234449108fd1e4a65d6b
hwalker928 <freelance@harrydev.me>
2021-05-14 22:37:46 +0100

    Update panel.sh

--- panel.sh
+++ panel.sh
@@ -1,18 +1,12 @@
-INFO='\033[0;36m'
-WARN='\033[0;31m'
-BRACKETS='\033[0;30m'
-info(){
-    echo -e "${BRACKETS}[${INFO}INFO${BRACKETS}]${1}";
-}
-warn(){
-    echo -e "${BRACKETS}[${WARN}WARN${BRACKETS}]${1}";
+output(){
+    echo -e '\e[36m'$1'\e[0m';
 }
 
-info "Please note, this only works for harrydev!"
+output "Please note, this only works for harrydev!"
 
 if [ $(whoami) = 'root' ]; then
-    info "Running as root.."
-    warn "Make sure the latest panel.tar.gz file is in /var/www/pterodactyl before continuing!"
+    output "Running as root.."
+    output "Make sure the latest panel.tar.gz file is in /var/www/pterodactyl before continuing!"
     read -p "Press enter to continue or CTRL+C to abort"
     export COMPOSER_ALLOW_SUPERUSER=1
     cd /var/www/pterodactyl
@@ -28,7 +22,7 @@ if [ $(whoami) = 'root' ]; then
     php artisan queue:restart
     php artisan up
     rm panel.tar.gz
-    info "Update finished."
+    output "Update finished."
 else
-    info "Please run this command as root."
+    output "Please run this command as root."
 fi
